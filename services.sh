44d4b09a518f3e1a25d63940635740d1d4682c73
Ventouz <potoroo725@gmail.com>
2018-04-18 06:24:57 +0200

    conversion du script

--- services.sh
+++ services.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo -e "Redémarrage des services..."
+echo -e "Redemarrage des services..."
 
-##  Démarrer les services linux de base.
-##  Ces services devraient être automatiquement démarrés.
+##  Demarrer les services linux de base.
+##  Ces services devraient etre automatiquement demarres.
 service cron start
 service mysql start
 service apache2 start
 
 
-##  Démarrer le programme Berceuse
+##  Demarrer le programme Berceuse
 # /root/berceuse.c
 
 
-## Jouer un son quand le Raspberry est opérationnel
+## Jouer un son quand le Raspberry est operationnel
