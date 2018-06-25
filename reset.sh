517422bb15f9005d96892cec96e0dca6b0cb4ce7
Ventouz <potoroo725@gmail.com>
2018-06-25 19:36:01 +0200

    préparation du script de remise à zéro

---
+++ reset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## Le but de ce script est de remettre certaines configurations
+## du Raspberry à leurs valeurs par défaut.
+
+# --------------------
+# Base de données
+# --------------------
+
+
+# --------------------
+# Configuration Web
+# --------------------
+
+
+# --------------------
+# Réseau
+# --------------------
+
+
+
+# --------------------
+# Services
+# --------------------
+
+
+# --------------------
+# GitHub
+# --------------------
