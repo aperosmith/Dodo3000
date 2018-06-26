46f4cca6925463c98054d3ce43a3a77c1bc2aed1
Ventouz <potoroo725@gmail.com>
2018-06-26 11:41:21 +0200

    Include /etc/phpmyadmin/apache.conf

--- init.sh
+++ init.sh
@@ -3,7 +3,15 @@
 log_path="/var/log/workshop"
 date=$(date +"%y-%m-%d")
 
+git config --global http.sslverify=false
+
+
+# --------------------
+# Apache
+# --------------------
+
 service apache2 stop
+echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
 rm -f /etc/apache2/sites-available/000-default.conf
 cp /root/Workshop/config/000-default.conf /etc/apache2/sites-available/
 service apache2 start
